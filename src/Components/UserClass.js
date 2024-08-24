988105ddea984fd4ba8bb77d6a26516317272496
Satish <satish.k@mobtexting.com>
2024-08-24 11:46:45 +0000

    react life cyclce

--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -1,5 +1,6 @@
 // import React from "react";
 import {Component} from "react";
+import Shimmer from "./Shimmer";
 
 // class UserClass extends React.Component
 class UserClass extends Component
@@ -9,24 +10,48 @@ class UserClass extends Component
         super(props);
         this.state = {
             count : 0,
-            count2: 2
+            count2: 2,
+            userInfo: null,
         }
         
-        console.log(this.props.name + 'child const');
+        // console.log(this.props.name + 'child const');
     }
 
-    componentDidMount(){
-        console.log(this.props.name  + 'component did mount');
+    async componentDidMount(){
+        // console.log(this.props.name  + 'component did mount');
+        const data = await fetch("https://api.github.com/users/nagasatishkadiyam");
+        const json = await data.json();
+        this.setState({
+            userInfo : json,
+        });
+    }
+
+    componentDidUpdate() {
+        // console.log('component did update will be called after render in the updating stage');
+    }
+
+    componentWillUnmount(){
+        // console.log("componentwillunmount will be called as soon as you going to different page(while changing to different menu)");
     }
 
     render() {
-        console.log(this.props.name +  'component render');
+        // console.log(this.props.name +  'component render');
+        // object destructring
         const {name, city} = this.props;
-        const {count, count2} = this.state;
+        const {count, count2, userInfo} = this.state;
+
+
+        if (userInfo === null) {
+            return <Shimmer></Shimmer>;
+        }
+
         return (
             <div className="about">
-                <h1>{name}</h1>
-                <h2>{city}</h2>
+                <h1>{userInfo?.name}</h1>
+                <h2>{userInfo?.login}</h2>
+                <h2>{userInfo?.location}</h2>
+                <img alt="not found" src={userInfo?.avatar_url} ></img>
+
                 <h2>count : {count}</h2>
                 <h2>count2 : {count2}</h2>
                 <button onClick={() => {
